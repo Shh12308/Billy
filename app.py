95ef9f2a9bbe4223dde8ef5469c54940cc09faa4
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2025-12-30 20:11:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,6 @@ import subprocess
 import tempfile
 from datetime import datetime
 from typing import Optional, Dict, Any, List
-from ultralytics.nn.tasks import DetectionModel
-
-torch.serialization.add_safe_globals([DetectionModel])
 from ultralytics import YOLO
 import cv2
 
