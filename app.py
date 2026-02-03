bb15acf88e036b42033fbd942ed23e6c3b48e16a
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-02-03 15:54:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -49,9 +49,6 @@ from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.executors.asyncio import AsyncIOExecutor
 from apscheduler.schedulers.base import STATE_RUNNING
 
-if scheduler.state != STATE_RUNNING:
-    scheduler.start()
-    
 # ---------- CONFIG & LOGGING ----------
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 logger = logging.getLogger("zynara-server")
