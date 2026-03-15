3cf5bb5f432eee376c332c890a32bca393b6c8cd
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-03-15 11:23:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ scheduler.start()
 
 async def stream():
     data = {"message": "hello"}
-    yield json.dumps(data) nmmmnm # ✅ convert to string
+    yield json.dumps(data)
 
 app = FastAPI(
     title="ZyNaraAI1.0 Multimodal Server",
