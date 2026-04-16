3bb1d4adf195eee834901eca3d4fcb4da6e6e376
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-04-16 18:22:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://heloxai.xyz"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
