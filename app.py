919340af5848d1d8fb5543512f566e22bd41fa25
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-08-11 16:19:21 +0100

    Restrict CORS to specific origins

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:9898","https://pwa-eight-wine.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
